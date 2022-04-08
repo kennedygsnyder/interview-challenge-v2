f90e4cd7fe4dddcf8f1955c9efbee5c643b05464
KyrinLee <kennedy.g.snyder@gmail.com>
2022-04-08 16:28:01 -0400

    Create GET '/states' route
     Create GET '/states' route to return data from states model

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,10 @@ const { State } = require('../models');
 // TODO: Create a GET '/states' route that returns data from your State model
 // Note: The functions to retrieve data from the database using sequelize is asyncronous, so it will look something like this:
 // var people = await Person.doSomething();
+router.get('/states', async (req, res) => {
+  const states = await State.findAll();
+  res.json(states); 
+})
+
 
 module.exports = router;
